ca66858f58a3f8f5e9fbc11b51530d4a57199cc8
Michael <28806828+PumPum7@users.noreply.github.com>
2022-07-24 21:55:09 +0200

    use state instead of refs for the form

--- project/pages/create.tsx
+++ project/pages/create.tsx
@@ -1,4 +1,4 @@
-import {useRef} from "react";
+import {useState} from "react";
 import ProfilePage from "../components/Profile";
 
 export default function Create() {
@@ -34,12 +34,12 @@ export default function Create() {
 		}
 	}
 
-	const nameRef = useRef<HTMLInputElement>(null)
-	const pictureRef = useRef<HTMLInputElement>(null)
-	const focusRef = useRef<HTMLInputElement>(null)
-	const hobbyRef = useRef<HTMLInputElement>(null)
-	const countryRef = useRef<HTMLInputElement>(null)
-	const jobRef = useRef<HTMLInputElement>(null)
+	const [nameState, setNameState] = useState("")
+	const [pictureState, setPictureState] = useState("")
+	const [focusState, setFocusState] = useState("")
+	const [hobbyState, setHobbyState] = useState("")
+	const [countryState, setCountryState] = useState("")
+	const [jobState, setJobState] = useState("")
 
 	return (
 		<main className="flex flex-col md:flex-row items-center justify-center h-screen gap-5">
@@ -49,39 +49,45 @@ export default function Create() {
 
 					<label htmlFor="name" className="text-lg">Name</label>
 					<input type="text" id="name" name="name" required placeholder="Pum" className="text-nord2 rounded-full p-2"
-					       ref={nameRef}/>
+					       value={nameState} onChange={e => setNameState(e.target.value)}/>
 
 					<label htmlFor="avatar" className="text-lg">A picture of you</label>
 					<input type="url" id="avatar" name="avatar" required placeholder="https://example.com/image.png"
 					       pattern="https://.*.png" title="The picture has to be a valid url ending with .png"
-					       className="text-nord2 rounded-full p-2" ref={pictureRef}/>
+					       className="text-nord2 rounded-full p-2" value={pictureState}
+					       onChange={e => setPictureState(e.target.value)}/>
 
 					<label htmlFor="job" className="text-lg">Your current job</label>
 					<input type="text" id="job" name="job" required placeholder="Developer"
-					       className="text-nord2 rounded-full p-2" ref={jobRef}/>
+					       className="text-nord2 rounded-full p-2" value={jobState}
+					       onChange={e => setJobState(e.target.value)}/>
 
 					<label htmlFor="focus" className="text-lg">Your current focus</label>
 					<input type="text" id="focus" name="focus" required placeholder="creating awesome stuff..."
-					       className="text-nord2 rounded-full p-2" ref={focusRef}/>
+					       className="text-nord2 rounded-full p-2" value={focusState}
+					       onChange={e => setFocusState(e.target.value)}/>
 
 					<label htmlFor="hobbies" className="text-lg">Your hobbies</label>
 					<input type="text" id="hobbies" name="hobbies" required placeholder="exploring the world"
-					       className="text-nord2 rounded-full p-2" ref={hobbyRef}/>
+					       className="text-nord2 rounded-full p-2" value={hobbyState}
+					       onChange={e => setHobbyState(e.target.value)}/>
 
 					<label htmlFor="country" className="text-lg">Your country</label>
 					<input type="text" id="country" name="country" required placeholder="America"
-					       className="text-nord2 rounded-full p-2" ref={countryRef}/>
+					       className="text-nord2 rounded-full p-2" value={countryState}
+					       onChange={e => setCountryState(e.target.value)}/>
 
 					<button type="submit" className="rounded-full bg-nord9 hover:shadow-xl p-2 text-nord5 mt-2">Submit</button>
 				</form>
 			</section>
 			<section>
 				<h1 className="text-2xl">Preview:</h1>
-				<ProfilePage name={nameRef.current ? nameRef.current.value : "<Name>"}
-				             job={jobRef.current ? jobRef.current.value : "<Job>"}
-				             country={countryRef.current ? countryRef.current.value : "<Country>"}
-				             focus={focusRef.current ? focusRef.current.value : "<Focus>"}
-				             hobbies={hobbyRef.current ? hobbyRef.current.value : "<hobbies>"} avatar={pictureRef.current ? pictureRef.current.value : "https://via.placeholder.com/200.png?text=Placeholder"}/>
+				<ProfilePage name={nameState || "<Name>"}
+				             job={jobState || "<Job>"}
+				             country={countryState || "<Country>"}
+				             focus={focusState || "<Focus>"}
+				             hobbies={hobbyState || "<hobbies>"}
+				             avatar={pictureState.endsWith(".png") && pictureState.startsWith("https://") ? pictureState : "https://via.placeholder.com/200.png?text=Placeholder"}/>
 			</section>
 		</main>
 	)
