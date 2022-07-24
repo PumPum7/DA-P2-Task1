e734aa1a9248fc072383feec6aecb97c9be153ee
Michael <28806828+PumPum7@users.noreply.github.com>
2022-07-24 22:17:53 +0200

    testing purposes

--- project/pages/index.tsx
+++ project/pages/index.tsx
@@ -1,16 +1,17 @@
 import {GetServerSideProps} from "next";
 
-export default function IndexPage() {
+export default function IndexPage(props: {host: string, url: string}) {
+	console.log(props)
 	return (
 		<div>Home</div>
 	)
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
 	return {
-		redirect: {
-			destination: "/Pum",
-			permanent: true
+		props: {
+			host: context.req.headers.host,
+			url: context.req.url
 		}
 	}
 }
