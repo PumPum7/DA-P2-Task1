cd881d693ad396ca8d88d404825c14e18c7f1e42
Michael <28806828+PumPum7@users.noreply.github.com>
2022-07-24 21:24:29 +0200

    eslint changes

--- project/components/Profile.tsx
+++ project/components/Profile.tsx
@@ -5,12 +5,15 @@ export default function ProfilePage(props: ProfileData) {
 	return (
 		<section className="flex flex-row md:gap-5 items-center">
 			<div>
-				<h1 className="text-2xl">Hey, I'm {props.name}</h1>
+				<h1 className="text-2xl">Hey, I&apos;m {props.name}</h1>
 				<p>I am a {props.job} from {props.country}.<br/>
 					I currently focus on {props.focus}<br/>
 					In my free time I like to {props.hobbies}</p>
 			</div>
-			<img src={props.avatar} width={200} height={200} className="rounded-full" alt={"Profile Image"}/>
+			<picture>
+				<source srcSet={props.avatar} type="image/png" />
+				<img src={props.avatar} width={200} height={200} className="rounded-full" alt={"Avatar Image"} />
+			</picture>
 		</section>
 	)
 }
